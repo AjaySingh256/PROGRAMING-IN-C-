38fb32e67ec883c506f6717cb242d701e9a81165
Ajay Singh <ajaysingh75121@gmail.com>
2025-01-14 20:55:03 +0530

    Create UpdationOfString.cpp

---
+++ STRING/UpdationOfString.cpp
@@ -0,0 +1,13 @@
+//Updataion of string
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str ="AJAY SINGH";
+    cout<<str<<endl;
+    str[3]='V';
+    cout<<str<<endl;
+}
+// ***output***
+// AJAY SINGH
+// AJAV SINGH
